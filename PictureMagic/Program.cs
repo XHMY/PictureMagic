2f83700cf0a61e3b0fbfbf38bafef29ed5de8305
ii∞‵筱幽〆 <zyf6@outlook.com>
2017-09-03 00:12:13 +0800

    Add project files.

---
+++ PictureMagic/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing.Imaging;
+using System.Drawing;
+using System.IO;
+
+namespace Graphics
+{
+    class Program
+    {
+        
+        static Bitmap ReadyMainPic()
+        {
+            string[] filePicName = Directory.GetFiles("Pic");
+            Bitmap tempPic = new Bitmap(filePicName[0]);
+            Bitmap mainPic = new Bitmap(tempPic.Width,tempPic.Height*filePicName.Length);
+            return mainPic;
+        }
+        static void CutPic(int x, int y, int width, int height)
+        {
+            string[] filePicName = Directory.GetFiles("Pic");
+            Rectangle cutArea = new Rectangle(x,y,width,height);
+            foreach (string PicName in filePicName)
+            {
+                Bitmap tempPic = new Bitmap(PicName);
+
+            }
+        }
+        static Dictionary<string,int> GetInput()
+        {
+            Dictionary<string,int> Area = new Dictionary<string,int>();
+            Console.WriteLine("Please input \"x\"");
+            Area.Add("x",Convert.ToInt16(Console.ReadLine()));
+            Console.WriteLine("Please input \"y\"");
+            Area.Add("y", Convert.ToInt16(Console.ReadLine()));
+            Console.WriteLine("Please input \"width\"");
+            Area.Add("width", Convert.ToInt16(Console.ReadLine()));
+            Console.WriteLine("Please input \"height\"");
+            Area.Add("height", Convert.ToInt16(Console.ReadLine()));
+            return Area;
+        }
+        static void Main(string[] args)
+        {
+            Dictionary<string,int> Area =  GetInput();
+            CutPic(Area["x"], Area["y"], Area["width"], Area["height"]);
+            Bitmap mainPic = ReadyMainPic();
+        }
+        
+    }
+}
